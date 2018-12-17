437b596958b50fa0a2d437b081158cf76859615b
Peckstadt Yves <peckstadt.yves@gmail.com>
2018-12-17 10:11:00 +0900

    Update builder.php

--- builder.php
+++ builder.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class QueryFilter {
+class QueryFieldFilter {
     public $fieldName;
     public $value;
 
@@ -11,12 +11,57 @@ class QueryFilter {
     }
 }
 
-abstract class Conditions
-{
+class QueryCondition {
+
+}
+
+
+class QueryCombination {
+    public $combinations = array();
+    public $filters = array();
+    public $conditions = array();
+    public $type;
+
+    public function __construct ($type, ...$criterias) {
+        $this->type = $type;
+        if (sizeof($criterias) < 2) {
+            throw new Exception("at least 2 criterias need to be provided");
+        }
+
+        foreach ($criterias as $criteria) {
+            if ($criteria instanceof QueryFieldFilter) {
+                array_push($this->filters, $criteria);
+            }
+            if ($criteria instanceof QueryCondition) {
+                array_push($this->conditions, $criteria);
+            }
+        }
+    }
+
+    public function addCombination($combination) {
+        array_push($this->combinations, $combination);
+        return $this;
+    }
+}
+
+abstract class Conditions {
     const GreaterThan = '$gt';
     const GreaterThanOrEqual = '$gte';
     const LowerThan = '$lt';
     const LowerThanOrEqual = '$lte';
+    const Equal = '$eq';
+    const NotEqual = '$neq';
+    const Exists = '$exists';
+    const Size = '$size';
+    const In = '$in';
+    const Type = '$type';
+    const Regex = '$regex';
+    const Mod = '$mod';
+}
+
+abstract  class Combinations {
+    const And = '$and';
+    const Or = '$or';
 }
 
 
@@ -30,11 +75,12 @@ class QueryBuilder {
     private $sort;
     private $filters = array();
     private $conditions;
+    private $combinations = array();
 
 
     public function __construct () {
-       $this->sort = new stdClass();
-       $this->conditions = new stdClass();
+        $this->sort = new stdClass();
+        $this->conditions = new stdClass();
     }
 
     function build() {
@@ -70,16 +116,20 @@ class QueryBuilder {
 
             // add conditions
             $conditions = get_object_vars($this->conditions);
-            if (sizeof($conditions) > 0) {
-                echo $conditions;
-//                $i = 0;
-//                foreach ($conditions as $condition) {
-//                    $selector->{$condition} = "dwfwfwdwdw";
-//                    $i++;
-//                }
+            $len = sizeof($conditions);
+            if ($len > 0) {
+                $properties = array_keys($conditions);
+                for ($i = 0; $i < $len ; $i++) {
+                    $selector->{$properties[$i]} = $conditions{$properties[$i]};
+                }
             }
 
             // combinations
+            if (sizeof($this->combinations) > 0) {
+               foreach ($this->combinations as $combination) {
+                   $this->addCombinationToRoot($selector, $combination);
+               }
+		    }
         }
 
         // add sort
@@ -111,7 +161,7 @@ class QueryBuilder {
     }
 
     function addFilter($fieldName, $value) {
-        array_push($this->filters, new QueryFilter($fieldName, $value));
+        array_push($this->filters, new QueryFieldFilter($fieldName, $value));
         $this->hasSelector = TRUE;
         return $this;
     }
@@ -145,12 +195,14 @@ class QueryBuilder {
         return $this;
     }
 
-    function addCombination() {
 
+    function addCombination($combination) {
+        array_push($this->combinations, $combination);
+        return $this;
     }
 
-    private function addCombinationToRoot() {
-
+    private function addCombinationToRoot($root, $combination) {
+        echo "addCombinationToRoot";
     }
 
     private function addCombinationToParent() {
@@ -159,12 +211,18 @@ class QueryBuilder {
 }
 
 $queryBuilder = new QueryBuilder();
+
+
+$combination = new QueryCombination(Combinations::And, new QueryFieldFilter("a", 1), new QueryFieldFilter("b", 2));
 $json = $queryBuilder
     ->addField("id", "name", "testing")
     ->setLimit(20)
     ->setSkip(20)
     ->addSort("id", "asc")
     ->addFilter("filterField", array(1,2,3))
-    ->addCondition(Conditions::GreaterThan, "id",1)
+    ->addCondition(Conditions::Regex, "id","fefee")
+    ->addCondition(Conditions::Mod, 'fefefe', [1,2])
+    ->addCondition(Conditions::In, "fullName", [1,2,3,4])
+    ->addCombination($combination)
     ->build();
 echo $json;
